7dee4831ec86815b5db5c511bebe87e7cc1af19d
TarTarSany <sany34391@gmail.com>
2026-07-03 00:29:23 +0300

    siteST1

--- MyMomReactSite/src/App.tsx
+++ MyMomReactSite/src/App.tsx
@@ -1,30 +1,67 @@
-import "./styles/main.scss";
-
+import "./styles/style.scss";
+import iconImg from "./styles/assets/image/icon.jpg";
+import St1foto from "./styles/assets/image/fotoSt1.jpg";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleTelegramClick = () => {
+    window.open("tg://resolve?domain=Olja10", "_blank", "noopener,noreferrer");
+  };
 
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src="public/logo.jpg" alt="Logo" className="Logo" />
-          <ul className="">
-            <li><a className="Search" href="#Search">Головна</a></li>
-            <li><a className="Delivery" href="#Delivery">Іграшки</a></li>
-            <li><a className="About" href="#About">Про нас</a></li>
-            <li><a className="Contact" href="#Contact">Контакти</a></li>
-            <button className="">
-              Написати нам
-              <img src="assets/images/contact-icon.jpg" className="icon01" alt="Contact" />
-            </button>
-          </ul>
-        </header>
-      </div>
-      <div className="bg-[#fbc3ca] absolute top-16 left-0 w-full h-[calc(100vh-4rem)] flex items-center justify-center">
+      {/* Хедер - окремий блок */}
+      <header className="App-header">
+        <button className="App-header_button" onClick={handleTelegramClick}>
+          <h3 className="App-header_button-text">Написати нам</h3>
+          <img src={iconImg} className="icon" alt="Telegram icon" />
+        </button>
 
+        <button 
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <ul className={`App-header_Pannel ${menuOpen ? "open" : ""}`}>
+          <li><a className="Search" href="#Search" onClick={() => setMenuOpen(false)}>Головна</a></li>
+          <li><a className="Delivery" href="#Delivery" onClick={() => setMenuOpen(false)}>Іграшки</a></li>
+          <li><a className="About" href="#About" onClick={() => setMenuOpen(false)}>Про нас</a></li>
+          <li><a className="Contact" href="#Contact" onClick={() => setMenuOpen(false)}>Контакти</a></li>
+        </ul>
+      </header>
+
+      {/* Основний контент - окремий блок, який йде ЗНИЗУ від хедера */}
+      <div className="App-body-ST1" id="Search">
+        <div className="App-body-ST1-left">
+          <h1 className="App-body-ST1-title">
+            Найкращі іграшки
+            <span className="App-body-ST1-titleSPAN">ручної роботи</span>
+          </h1>
+          <h3 className="App-body-ST1-subtitle">М'які, безпечні, та створені з любов'ю</h3>
+          <h3 className="App-body-ST1-subtitle">Найдіть ідеальну іграшку для вашої дитини</h3>
+          <button 
+            className="App-body-ST1-button"
+            onClick={() => window.location.hash = 'Delivery'}
+          >
+          <span className="App-body-ST1-button_text">вибрати іграшку</span>
+          </button>
+          <button className="App-header_buttonST1" onClick={handleTelegramClick}>
+            <h3 className="App-header_button-text">Написати нам</h3>
+            <img src={iconImg} className="icon" alt="Telegram icon" />
+          </button>
+        </div>
+        <div className="App-body-ST1-right">
+          <img src={St1foto} alt="ST1 image" />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
